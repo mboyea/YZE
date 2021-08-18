05d814f9e3ea4dae6ad2304b4e3cc51a426732e0
YourZombieMop <68197922+YourZombieMop@users.noreply.github.com>
2021-08-17 19:21:11 -0500

    Collider
    Worked on collisions - currently collider offsets aren't working correctly.

--- YourZombieEngine/BoxCollider.cpp
+++ YourZombieEngine/BoxCollider.cpp
@@ -19,165 +19,118 @@ Vector2i GetLineIntersection(const Vector2i& a, const Vector2i& b, const Vector2
 	return { ((b2 * c1) - (b1 * c2)) / determinant,((a1 * c2) - (a2 * c1)) / determinant };
 }
 
+int UnsafeGetLineIntersectionX(const Vector2i& a, const Vector2i& b, const Vector2i& c, const Vector2i& d) {
+	// line a-b represented as a1x + b1y = c1
+	const int a1 = b.y - a.y;
+	const int b1 = a.x - b.x;
+	const int c1 = (a1 * a.x) + (b1 * a.y);
+	// line c-d represented as a2x + b2y = c2
+	const int a2 = d.y - c.y;
+	const int b2 = c.x - d.x;
+	const int c2 = (a2 * c.x) + (b2 * c.y);
+	return ((b2 * c1) - (b1 * c2)) / ((a1 * b2) - (a2 * b1));
+}
+
 Collision BoxCollider::DoNarrowCollision(Transform* transform, Collider* target, Transform* targetTransform) {
 	{ // target is BoxCollider
 		if (dynamic_cast<BoxCollider*>(target) != nullptr) {
 			if (doSolveCollision) {
-				const Vector2i offset = transform->pos - transform->lastPos;
-				const Vector2i leadingPoint = {
-					aabb.x + ((offset.x > 0) * aabb.w),
-					aabb.y + ((offset.y > 0) * aabb.h)
-				};
-				const Vector2i pathStart = transform->lastPos + leadingPoint;
-				const Vector2i pathEnd = transform->pos + leadingPoint;
-				const Vector2i topLeft = targetTransform->pos + GetAABB().GetPos();
-				const Vector2i topRight = {
-					targetTransform->pos.x + GetAABB().GetPos().x + GetAABB().GetDim().x,
-					targetTransform->pos.y + GetAABB().GetPos().y
-				};
-				const Vector2i bottomLeft = {
-					targetTransform->pos.x + GetAABB().GetPos().x,
-					targetTransform->pos.y + GetAABB().GetPos().y + GetAABB().GetDim().y
-				};
-				const Vector2i bottomRight = {
-					targetTransform->pos.x + GetAABB().GetPos().x + GetAABB().GetDim().x,
-					targetTransform->pos.y + GetAABB().GetPos().y + GetAABB().GetDim().y
-				};
-				std::pair<Vector2i, Vector2i> impulse;
-				if (pathStart.y < pathEnd.y) {
-					// can hit top, left, or right
-					if (pathStart.x < pathEnd.x) {
-						// can hit left or top
-						if (GetLineIntersection(pathStart, pathEnd, topLeft, topRight).x >= topLeft.x) {
-							// will hit top
-							impulse.first = {
-								0,
-								targetTransform->pos.y - pathEnd.y
-							};
-							transform->pos = transform->pos + impulse.first;
-							return Collision(true, impulse);
-						}
-						{
-							// will hit left
-							impulse.first = {
-								targetTransform->pos.x - pathEnd.x,
-								0
-							};
-							transform->pos = transform->pos + impulse.first;
-							return Collision(true, impulse);
-						}
-					}
-					if (pathStart.x > pathEnd.x) {
-						// can hit right or top
-						if (GetLineIntersection(pathStart, pathEnd, topLeft, topRight).x <= topRight.x) {
-							// will hit top
-							impulse.first = {
-								0,
-								targetTransform->pos.y - pathEnd.y
-							};
-							transform->pos = transform->pos + impulse.first;
-							return Collision(true, impulse);
-						}
-						{
-							// will hit right
-							impulse.first = {
-								targetTransform->pos.x - pathEnd.x + target->GetAABB().w,
-								0
-							};
-							transform->pos = transform->pos + impulse.first;
-							return Collision(true, impulse);
-						}
-					}
-					{
-						// will hit top
-						impulse.first = {
-							0,
-							targetTransform->pos.y - pathEnd.y
-						};
-						transform->pos = transform->pos + impulse.first;
-						return Collision(true, impulse);
-					}
-				}
-				if (pathStart.y > pathEnd.y) {
-					// can hit bottom, left, or right
-					if (pathStart.x < pathEnd.x) {
-						// can hit left or bottom
-						if (GetLineIntersection(pathStart, pathEnd, bottomLeft, bottomRight).x >= bottomLeft.x) {
-							// will hit bottom
-							impulse.first = {
-								0,
-								targetTransform->pos.y - pathEnd.y + target->GetAABB().w
-							};
-							transform->pos = transform->pos + impulse.first;
-							return Collision(true, impulse);
-						}
-						{
-							// will hit left
-							impulse.first = {
-								targetTransform->pos.x - pathEnd.x,
-								0
-							};
-							transform->pos = transform->pos + impulse.first;
-							return Collision(true, impulse);
-						}
-					}
-					if (pathStart.x > pathEnd.x) {
-						// can hit right or bottom
-						if (GetLineIntersection(pathStart, pathEnd, bottomLeft, bottomRight).x <= bottomRight.x) {
-							// will hit bottom
-							impulse.first = {
-								0,
-								targetTransform->pos.y - pathEnd.y + target->GetAABB().w
-							};
-							transform->pos = transform->pos + impulse.first;
-							return Collision(true, impulse);
-						}
-						{
-							// will hit right
-							impulse.first = {
-								targetTransform->pos.x - pathEnd.x + target->GetAABB().w,
-								0
-							};
-							transform->pos = transform->pos + impulse.first;
-							return Collision(true, impulse);
-						}
-					}
-					{
-						// will hit bottom
-						impulse.first = {
-							0,
-							targetTransform->pos.y - pathEnd.y + target->GetAABB().w
-						};
-						transform->pos = transform->pos + impulse.first;
-						return Collision(true, impulse);
-					}
-				}
-				{
-					// can hit left or right
-					if (pathStart.x < pathEnd.x) {
-						// will hit left
-						impulse.first = {
-							targetTransform->pos.x - pathEnd.x,
-							0
-						};
-						transform->pos = transform->pos + impulse.first;
-						return Collision(true, impulse);
-					}
-					{
-						// will hit right
-						impulse.first = {
-							targetTransform->pos.x - pathEnd.x + target->GetAABB().w,
-							0
-						};
-						transform->pos = transform->pos + impulse.first;
-						return Collision(true, impulse);
-					}
-				}
-				
 				//	impulse.first = {
 				//		int(/*is horizontal*/) * (targetTransform->pos.x - pathEnd.x + int(/*is right*/offset.x < 0) * (target->GetAABB().w)),
 				//		int(/*is vertical*/) * (targetTransform->pos.y - pathEnd.y + int(/*is bottom*/offset.y < 0) * (target->GetAABB().h))
 				//	};
+				std::pair<Vector2i, Vector2i> impulse = { { 0, 0 }, { 0, 0 } };
+				// calculate the position of lhs
+				const Vector2i lhsStartPos = transform->lastPos + aabb.GetPos();
+				const Vector2i lhsEndPos = transform->pos + aabb.GetPos();
+				// calculate the position of rhs
+				const Vector2i rhsStartPos = targetTransform->lastPos + target->GetAABB().GetPos();
+				const Vector2i rhsEndPos = targetTransform->pos + target->GetAABB().GetPos();
+				// calculate the leading point of the lhs rectangle in local space
+				const Vector2i lhsLocalLeadingPoint = {
+					aabb.x + int(transform->pos.x - transform->lastPos.x > 0) * (aabb.w),
+					aabb.y + int(transform->pos.y - transform->lastPos.y > 0) * (aabb.h)
+				};
+				// calculate the [leading point of the lhs rectangle]'s start & end points
+				const Vector2i lhsPathStart = lhsStartPos + lhsLocalLeadingPoint;
+				const Vector2i lhsPathEnd = lhsEndPos + lhsLocalLeadingPoint;
+				// determine the impulse required to resolve the collision based upon the path of the leading point
+				if (lhsPathStart.y < lhsPathEnd.y) {
+					// hit TOP, LEFT, or RIGHT
+					const Vector2i topLeft = rhsEndPos;
+					const Vector2i topRight = {
+						rhsEndPos.x + target->GetAABB().GetDim().x,
+						rhsEndPos.y
+					};
+					if (lhsPathStart.x < lhsPathEnd.x) {
+						// hit TOP or LEFT
+						if (UnsafeGetLineIntersectionX(lhsPathStart, lhsPathEnd, topLeft, topRight) >= topLeft.x) {
+							// hit TOP
+							impulse.first.y = rhsEndPos.y + aabb.GetPos().y - lhsPathEnd.y;
+						} else {
+							// hit LEFT
+							impulse.first.x = rhsEndPos.x + aabb.GetPos().x - lhsPathEnd.x;
+						}
+					} else if (lhsPathStart.x > lhsPathEnd.x) {
+						// hit TOP or RIGHT
+						if (UnsafeGetLineIntersectionX(lhsPathStart, lhsPathEnd, topLeft, topRight) <= topRight.x) {
+							// hit TOP
+							impulse.first.y = rhsEndPos.y + aabb.GetPos().y - lhsPathEnd.y;
+						} else {
+							// hit RIGHT
+							impulse.first.x = rhsEndPos.x + aabb.GetPos().x - lhsPathEnd.x + target->GetAABB().w;
+						}
+					} else {
+						// hit TOP
+						impulse.first.y = rhsEndPos.y + aabb.GetPos().y - lhsPathEnd.y;
+					}
+				} else if (lhsPathStart.y > lhsPathEnd.y) {
+					// hit BOTTOM, LEFT, or RIGHT
+					const Vector2i bottomLeft = {
+						rhsEndPos.x,
+						rhsEndPos.y + target->GetAABB().GetDim().y
+					};
+					const Vector2i bottomRight = {
+						rhsEndPos.x + target->GetAABB().GetDim().x,
+						rhsEndPos.y + target->GetAABB().GetDim().y
+					};
+					if (lhsPathStart.x < lhsPathEnd.x) {
+						// hit BOTTOM or LEFT
+						if (UnsafeGetLineIntersectionX(lhsPathStart, lhsPathEnd, bottomLeft, bottomRight) >= bottomLeft.x) {
+							// hit BOTTOM
+							impulse.first.y = rhsEndPos.y + aabb.GetPos().y - lhsPathEnd.y + target->GetAABB().h;
+						} else {
+							// hit LEFT
+							impulse.first.x = rhsEndPos.x + aabb.GetPos().x - lhsPathEnd.x;
+						}
+					} else if (lhsPathStart.x > lhsPathEnd.x) {
+						// hit BOTTOM or RIGHT
+						if (UnsafeGetLineIntersectionX(lhsPathStart, lhsPathEnd, bottomLeft, bottomRight) <= bottomRight.x) {
+							// hit BOTTOM
+							impulse.first.y = rhsEndPos.y + aabb.GetPos().y - lhsPathEnd.y + target->GetAABB().h;
+						} else {
+							// hit RIGHT
+							impulse.first.x = rhsEndPos.x + aabb.GetPos().x - lhsPathEnd.x + target->GetAABB().w;
+						}
+					} else {
+						// hit BOTTOM
+						impulse.first.y = rhsEndPos.y + aabb.GetPos().y - lhsPathEnd.y + target->GetAABB().h;
+					}
+				} else {
+					// hit LEFT or RIGHT
+					if (lhsPathStart.x < lhsPathEnd.x) {
+						// hit LEFT
+						impulse.first.x = rhsEndPos.x + aabb.GetPos().x - lhsPathEnd.x;
+					}
+					else if (lhsPathStart.x > lhsPathEnd.x) {
+						// hit RIGHT
+						impulse.first.x = rhsEndPos.x + aabb.GetPos().x - lhsPathEnd.x + target->GetAABB().w;
+					} else {
+						// TODO: move this object based upon the movement of the other object
+					}
+				}
+				transform->pos = transform->pos + impulse.first;
+				return Collision(true, impulse);
 			}
 			return Collision(true);
 		}
